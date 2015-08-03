5f47167242d7efbf39fdc52f472a5c410c6b6df1
Magnus Görlitz <magnus.goerlitz@googlemail.com>
2015-08-03 14:38:17 +0200

    Improve mastermind

--- cis194/02-lists/mastermind.hs
+++ cis194/02-lists/mastermind.hs
@@ -1,40 +1,39 @@
-data Peg = Red | Green | Blue | Yellow | Orange | Purple
-  deriving (Eq, Show)
-
-data Move = Move Code Int Int 
-  deriving (Eq, Show)
-
+data Peg = Red | Green | Blue | Yellow | Orange | Purple deriving (Eq, Show)
 type Code = [Peg]
+data Move = Move Code Int Int deriving (Eq, Show)
 
+colors :: Code
 colors = [Red, Green, Blue, Yellow, Orange, Purple]
 
 exactMatches :: Code -> Code -> Int
 exactMatches [] [] = 0
 exactMatches (x:xs) (y:ys)
-  | x == y = 1 + exactMatches xs ys
+  | x == y    = 1 + exactMatches xs ys
   | otherwise = exactMatches xs ys
 
 countColors :: Code -> [Int]
-countColors xs = map (\x -> exactMatches (replicate (length xs) x) xs) colors
+countColors xs = map count colors
+  where count x = exactMatches (replicate (length xs) x) xs
 
 matches :: Code -> Code -> Int
-matches xs ys = min (countColors xs) (countColors ys)
-  where min (x:xs) (y:ys) = minimum [x, y] + min xs ys
-        min [] [] = 0
+matches xs = loop (countColors xs) . countColors
+  where loop (x:xs) (y:ys) = min x y + loop xs ys
+        loop _      _      = 0
 
 getMove :: Code -> Code -> Move
-getMove x y = Move y (exactMatches x y) (matches x y - exactMatches x y)
+getMove x y = let exact = exactMatches x y
+              in  Move y exact $ matches x y - exact
 
 isConsistent :: Move -> Code -> Bool
-isConsistent m@(Move xs a b) ys = getMove ys xs == m
+isConsistent m@(Move xs a b) = (==) m . flip getMove xs
 
 allCodes :: Int -> [Code] 
 allCodes 2 = sequence [colors, colors]
-allCodes n = [(x:y) | x <- colors, y <- codes]
-  where codes = allCodes (n - 1)
+allCodes n = let codes = allCodes $ n - 1
+             in  [ x : y | x <- colors, y <- codes ]
 
 solve :: Code -> [Move]
-solve code = guess [] possibilities 
-  where guess moves (x:xs) | x == code = moves
-                           | otherwise = guess (moves ++ getMove x (head xs))  xs
-        possibilities = allCodes (length code)
+solve code = guess (allCodes $ length code) []
+    where guess (x:xs) moves
+            | x == code = moves
+            | otherwise = guess xs $ moves ++ [getMove x $ head xs]
