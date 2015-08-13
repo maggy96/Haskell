a33470c4ddea380b63fe65cb4346e4361ab15c63
Magnus Görlitz <magnus.goerlitz@googlemail.com>
2015-08-14 00:12:37 +0200

    Progress on HW04

--- cis194/04-typeclasses/hw04.hs
+++ cis194/04-typeclasses/hw04.hs
@@ -47,8 +47,8 @@ times (P p1) (P p2) = foldl (+) (P [0]) $ multiply p1 p2
 instance Num a => Num (Poly a) where
     (+) = plus
     (*) = times
-    negate      = undefined
-    fromInteger = undefined
+    negate      (P p1) = P $ map negate p1
+    fromInteger n      = P [fromInteger n]
     -- No meaningful definitions exist
     abs    = undefined
     signum = undefined
@@ -56,14 +56,19 @@ instance Num a => Num (Poly a) where
 -- Exercise 7 -----------------------------------------
 
 applyP :: Num a => Poly a -> a -> a
-applyP = undefined
+applyP (P p1) n = ap1 p1 0
+    where 
+        ap1 (p:ps) h = p * n ^ h + ap1 ps (h + 1)
+        ap1 [] _ = 0
 
 -- Exercise 8 -----------------------------------------
 
 class Num a => Differentiable a where
     deriv  :: a -> a
+    deriv  = undefined --(p:ps) = ps
     nderiv :: Int -> a -> a
-    nderiv = undefined
+    nderiv 1 n = deriv n
+    nderiv n s = nderiv (n - 1) s
 
 -- Exercise 9 -----------------------------------------
 
