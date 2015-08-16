c34153f8807fd71e27aea1dcc229b0e78ba885de
Magnus Görlitz <magnus.goerlitz@googlemail.com>
2015-08-16 15:51:02 +0200

    Begin HW05

---
+++ cis194/05-IO/HW05.hs
@@ -0,0 +1,105 @@
+{-# OPTIONS_GHC -Wall #-}
+{-# LANGUAGE OverloadedStrings, RecordWildCards #-}
+module HW05 where
+
+import Data.ByteString.Lazy (ByteString)
+import Data.Map.Strict (Map)
+import System.Environment (getArgs)
+
+import qualified Data.ByteString.Lazy as BS
+import qualified Data.Map.Strict as Map
+
+import Parser
+import Data.Bits
+
+-- Exercise 1 -----------------------------------------
+
+getSecret :: FilePath -> FilePath -> IO ByteString
+getSecret a b = do
+  afile <- BS.readFile a
+  bfile <- BS.readFile b
+  let result = BS.pack $ filter (/= 0) $ BS.zipWith (xor) afile bfile
+  return result
+
+-- Exercise 2 -----------------------------------------
+
+decryptWithKey :: ByteString -> FilePath -> IO ()
+decryptWithKey pw f = do
+  file <- BS.readFile (f ++ ".enc")
+  let res = BS.pack $ BS.zipWith (xor) file (BS.cycle pw)
+  BS.writeFile f res
+
+-- Exercise 3 -----------------------------------------
+
+parseFile :: FromJSON a => FilePath -> IO (Maybe a)
+parseFile file = do
+  json <- BS.readFile file
+  return $ decode json
+
+-- Exercise 4 -----------------------------------------
+
+getBadTs :: FilePath -> FilePath -> IO (Maybe [Transaction])
+getBadTs victimsPath transactionsPath = do
+  victims <- parseFile victimsPath :: IO (Maybe [TId])
+  transactions <- parseFile transactionsPath :: IO (Maybe [Transaction])
+  return $ filterT victims transactions
+
+filterT :: Maybe [TId] -> Maybe [Transaction] -> Maybe [Transaction]
+filterT (Just v) (Just t) = 
+  Just $ filter (\x -> elem (tid x) v) t
+filterT _ _ = Nothing
+
+-- Exercise 5 -----------------------------------------
+
+getFlow :: [Transaction] -> Map String Integer
+getFlow = undefined
+
+-- Exercise 6 -----------------------------------------
+
+getCriminal :: Map String Integer -> String
+getCriminal = undefined
+
+-- Exercise 7 -----------------------------------------
+
+undoTs :: Map String Integer -> [TId] -> [Transaction]
+undoTs = undefined
+
+-- Exercise 8 -----------------------------------------
+
+writeJSON :: ToJSON a => FilePath -> a -> IO ()
+writeJSON = undefined
+
+-- Exercise 9 -----------------------------------------
+
+doEverything :: FilePath -> FilePath -> FilePath -> FilePath -> FilePath
+             -> FilePath -> IO String
+doEverything dog1 dog2 trans vict fids out = do
+  key <- getSecret dog1 dog2
+  decryptWithKey key vict
+  mts <- getBadTs vict trans
+  case mts of
+    Nothing -> error "No Transactions"
+    Just ts -> do
+      mids <- parseFile fids
+      case mids of
+        Nothing  -> error "No ids"
+        Just ids -> do
+          let flow = getFlow ts       
+          writeJSON out (undoTs flow ids)
+          return (getCriminal flow)
+
+main :: IO ()
+main = do
+  args <- getArgs
+  crim <- 
+    case args of
+      dog1:dog2:trans:vict:ids:out:_ ->
+          doEverything dog1 dog2 trans vict ids out
+      _ -> doEverything "dog-original.jpg"
+                        "dog.jpg"
+                        "transactions.json"
+                        "victims.json"
+                        "new-ids.json"
+                        "new-transactions.json"
+  putStrLn crim
+
