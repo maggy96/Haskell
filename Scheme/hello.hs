e3f113f69801142374a0c3c2a618979b47eaf860
Magnus Görlitz <magnus.goerlitz@googlemail.com>
2016-08-12 12:47:54 +0200

    Move scheme to external Repo

--- Scheme/hello.hs
+++
@@ -1,9 +0,0 @@
-module Main where
-import System.Environment
-
-main :: IO()
-main = do args <- getArgs
-          putStrLn $ show ((read $ args !! 0) + (read $ args !! 1))
-          putStrLn "What's ya name?"
-          name <- getLine
-          putStrLn (name ++ " is a nice name.")
--- Scheme/readme.md
+++
@@ -1,3 +0,0 @@
-This folder is for "Write yourself a Scheme in 48 hours"
-
-https://en.wikibooks.org/wiki/Write_Yourself_a_Scheme_in_48_Hours
